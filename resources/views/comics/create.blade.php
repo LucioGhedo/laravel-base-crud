e035aef17be7f59af03c64e3affa10437f39f54b
LucioGhedo <lucioghedina@gmail.com>
2022-08-30 16:33:57 +0200

    form

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>Aggiungi prodotto</h1>
+
+    <form action="{{ route('comics.store') }}" method="POST">
+        @csrf
+        
+        <div>
+            <label for="title">titolo</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <br>
+        <div>
+            <label for="description">descrizione</label>
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+        <br>
+        <div>
+            <label for="thumb">url immagine</label>
+            <input type="text" name="thumb" id="thumb">
+        </div>
+        <br>
+        <div>
+            <label for="price">prezzo</label>
+            <input type="text" name="price" id="price">
+        </div>
+        <br>
+        <div>
+            <label for="series">serie</label>
+            <input type="text" name="series" id="series">
+        </div>
+        <br>
+        <div>
+            <label for="sale_date">prima pubblicazione</label>
+            <input type="text" name="sale_date" id="sale_date">
+        </div>
+        <br>
+        <div>
+            <label for="type">tipo del comic</label>
+            <input type="text" name="type" id="type">
+        </div>
+        <br>
+        <button>Salva</button>
+    </form>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>Ciao</h1>
+    <h1>Homepage</h1>
 @endsection
